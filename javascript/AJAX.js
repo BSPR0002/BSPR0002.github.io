cee3c0578bf50ad94ae42c5db3073d49305864ce
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2019-12-17 02:59:54 +0800

    测试器

--- javascript/AJAX.js
+++ javascript/AJAX.js
@@ -1,14 +1,18 @@
-function AJAXstandardLOAD(targ) {
-	var xmlhttp;
+function AJAX(targ,rqtype) {
+	var xmlhttp=null;
+	//IE兼容
 	if (window.XMLHttpRequest) {
 		xmlhttp=new XMLHttpRequest();
 	} else {
 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 	}
+	//兼容代码尾部
 	xmlhttp.onreadystatechange=function() {
 		if (xmlhttp.readyState==4 && xmlhttp.status==200) {
+			console.log("=1=");
 			console.log(xmlhttp.responseText);
-			console.log(xmlhttp.responsexXML);
+			console.log("=2=");
+			console.log(xmlhttp.responseXML);
 			//this.innerHTML=xmlhttp.responseXML;
 		}
 	}
