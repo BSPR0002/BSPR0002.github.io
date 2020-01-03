e182cde5e8be8fcba99109e2dbfacc1538551c61
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2020-01-03 21:50:48 +0800

    修复列表解析器

--- javascript/Library.js
+++ javascript/Library.js
@@ -24,21 +24,22 @@ function OverView(library) {
 function Show() {
 	$.each(ShowData,function(i,obj) {
 		var Card=document.createElement("div");
-		Card.class="card";
-		var CardIcon=document.createElement("div");
-		CardIcon.class="card_icon";
+		Card.className="card";
+		var CardIcon=document.createElement("img");
+		CardIcon.className="card_icon";
+		CardIcon.src=obj.icon;
 		Card.appendChild(CardIcon);
 		var CardName=document.createElement("p");
-		CardName.class="card_name";
+		CardName.className="card_name";
 		var Node=document.createTextNode(obj.display);
 		CardName.appendChild(Node);
 		Card.appendChild(CardName);
 		var CardType=document.createElement("p");
-		CardType.class="card_type";
+		CardType.className="card_type";
 		switch (obj.type) {
 			case "allinone":
 			var allinone="合集（";
-			$.each(obj.Allinone,function(i,val) {
+			$.each(obj.AllInOne,function(i,val) {
 				if (allinone!="合集（") allinone=allinone+"、";
 				allinone=allinone+val;
 			})
@@ -48,6 +49,15 @@ function Show() {
 		}
 		CardType.appendChild(Node);
 		Card.appendChild(CardType);
-		document.getElementById("res_box").appendChild(Card);
+		document.getElementById("show_box").appendChild(Card);
 	});
 }
+
+function Beginning() {
+	if (document.getElementById("show_box")) {
+		window.clearInterval(BeginID);
+		OverView(document.getElementById("show_box").className);
+	}
+}
+
+var BeginID=window.setInterval("Beginning()",100);
--- javascript/TestFunction.js
+++ javascript/TestFunction.js
@@ -1,5 +1,21 @@
-function testfunc2(){
-	$("#targetdiv").load("/html/test_page.html");
+var LibraryData=[
+	{
+		"ID":1,
+		"name":"Nekopara",
+		"display":"ネコぱら",
+		"icon":"/Images/resource_icon/ID00000001.png",
+		"type":"allinone",
+		"AllInOne":["PC版游戏","手机版游戏","Steam R18 DLC","解包","动画","周边"],
+		"resource":{
+			"BDND":null,
+			"Torrent":null
+		}
+	}
+];
+CurrentLibrary="resource";
+
+function testfunc(){
+	OverView("resource");
 }
 
 function PullLibrary(library) {
