9f786641ab0fae0c056cd0eb80bdfd91df987a11
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2020-01-06 03:30:43 +0800

    资源上线

--- javascript/TestFunction.js
+++ javascript/TestFunction.js
@@ -7,15 +7,40 @@ var LibraryData=[
 		"type":"allinone",
 		"AllInOne":["PC版游戏","手机版游戏","Steam R18 DLC","解包","动画","周边"],
 		"resource":{
-			"BDND":null,
-			"Torrent":null
+			"BDND":{"link":"https://pan.baidu.com/s/1kId1GKYVgqD66AMtRrcTwA","password":"e1s7"},
+			"Torrent":"EFE"
+		}
+	},
+	{
+		"ID":2,
+		"name":"Nekopara",
+		"display":"ネコぱら",
+		"icon":"/Images/resource_icon/ID00000001.png",
+		"type":"allinone",
+		"AllInOne":["PC版游戏","手机版游戏","Steam R18 DLC","解包","动画","周边"],
+		"resource":{
+			"BDND":{"link":"https://pan.baidu.com/s/1kId1GKYVgqD66AMtRrcTwA","password":"e1s7"},
+			"Torrent":"EFE"
+		}
+	},
+	{
+		"ID":3,
+		"name":"Nekopara",
+		"display":"ネコぱら",
+		"icon":"/Images/resource_icon/ID00000001.png",
+		"type":"allinone",
+		"AllInOne":["PC版游戏","手机版游戏","Steam R18 DLC","解包","动画","周边"],
+		"resource":{
+			"BDND":{"link":"https://pan.baidu.com/s/1kId1GKYVgqD66AMtRrcTwA","password":"e1s7"},
+			"Torrent":"EFE"
 		}
 	}
 ];
-CurrentLibrary="resource";
 
-function PullLibrary(library) {
-	$.getJSON("/json/"+library+".json",function(resp) {
-		LibraryData=resp;
-	})
+function testfunc() {
+	var targetE=document.getElementsByClassName("exp");
+	console.log(targetE);
+	targetE.info=22;
+	console.log(targetE.info);
 }
+
