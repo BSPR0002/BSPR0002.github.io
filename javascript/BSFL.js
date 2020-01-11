f2099846f1ed218994d02c9132164210feff2d2e
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2020-01-12 00:38:40 +0800

    Update BSFL.js

--- javascript/BSFL.js
+++ javascript/BSFL.js
@@ -2,7 +2,7 @@ function AJAX(parameter) {
 	var model={"method":"get","url":null,"async":true,"username":null,"password":null,"send":null,"cache":true,"success":null,"fail":null};
 	Object.assign(model,parameter);
 	var XMLHR=new XMLHttpRequest();
-	XMLHR.open(model.open,model.url,model.async,model.username,model.password);
+	XMLHR.open(model.method,model.url,model.async,model.username,model.password);
 	if (model.cache==false) XMLHR.setRequestHeader("If-Modified-Since","0");
 	XMLHR.onreadystatechange=function() {
 		if (XMLHR.readyState==4) {
