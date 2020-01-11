53cc161563565c7e70584d46d68092f527760def
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2020-01-12 00:32:57 +0800

    Update BSFL.js

--- javascript/BSFL.js
+++ javascript/BSFL.js
@@ -1,3 +1,17 @@
-function ajax() {
-	alert("未完成");
+function AJAX(parameter) {
+	var model={"method":"get","url":null,"async":true,"username":null,"password":null,"send":null,"cache":true,"success":null,"fail":null};
+	Object.assign(model,parameter);
+	var XMLHR=new XMLHttpRequest();
+	XMLHR.open(model.open,model.url,model.async,model.username,model.password);
+	if (model.cache==false) XMLHR.setRequestHeader("If-Modified-Since","0");
+	XMLHR.onreadystatechange=function() {
+		if (XMLHR.readyState==4) {
+			if (XMLHR.status==200||XMLHR.status==304) {
+				model.success(XMLHR.responseText);
+			} else if (typeof model.fail=="function") {
+				model.fail();
+			} else model.fail;
+		};
+	};
+	XMLHR.send(model.send);
 }
