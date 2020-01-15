f3900e1a511a49d674a363da8f72e9f512285117
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2020-01-16 02:23:33 +0800

    9

--- javascript/BSFL.js
+++ javascript/BSFL.js
@@ -1,30 +1,30 @@
 function AJAX(parameter) {
-	var model={"method":"get","url":null,"async":true,"username":null,"password":null,"send":null,"cache":true,"success":function(){},"fail":function(){}};
+	var model={"method":"get","url":null,"async":true,"username":undefined,"password":null,"type":"","timeout":0,"send":null,"cache":true,"success":function(){},"fail":function(){}};
 	Object.assign(model,parameter);
-	var XMLHR=new XMLHttpRequest();
-	XMLHR.open(model.method,model.url,model.async,model.username,model.password);
-	if (model.cache==false) XMLHR.setRequestHeader("If-Modified-Since","0");
-	XMLHR.onreadystatechange=function() {
-		if (XMLHR.readyState==4) {
-			if (XMLHR.status==200||XMLHR.status==304) {
-				model.success(XMLHR.responseText);
-			} else model.fail(XMLHR.status);
-		};
+	var XHR=new XMLHttpRequest();
+	XHR.open(model.method,model.url,model.async,model.username,model.password);
+	XHR.responseType=model.type;
+	XHR.timeout=model.timeout;
+	if (model.cache==false) XHR.setRequestHeader("If-Modified-Since","0");
+	XHR.onload=function() {
+		if ((XHR.status>=200&&XHR.status<300)||XHR.status==304) {
+			model.success(XHR.response);
+		} else model.fail(XHR.status);
 	};
-	XMLHR.send(model.send);
+	XHR.send(model.send);
 }
 
 function getJSON(url,callback,AllowCache){
-	var AJAXModel={"url":url,"success":function(response) {
-		callback(JSON.parse(response));
+	var AJAXModel={"url":url,"type":"json","success":function(response) {
+		callback(response);
 	}};
 	if (AllowCache==false) AJAXModel.cache=false;
 	AJAX(AJAXModel);
 }
 
 function getXML(url,callback,AllowCache) {
-	var AJAXModel={"url":url,"success":function(response) {
-		callback(new DOMParser().parseFromString(response,"text/xml"));
+	var AJAXModel={"url":url,"type":"document","success":function(response) {
+		callback(response);
 	}};
 	if (AllowCache==false) AJAXModel.cache=false;
 	AJAX(AJAXModel);
@@ -39,6 +39,7 @@ function EmptyElement(TargetElement) {
 function Load(url,TargetElement,AllowCache) {
 	var AJAXModel={"url":url,"success":function(response) {
 		var Operator=document.createRange().createContextualFragment(response);
+		EmptyElement(TargetElement);
 		TargetElement.appendChild(Operator);
 	}};
 	if (AllowCache==false) AJAXModel.cache=false;
@@ -48,3 +49,4 @@ function Load(url,TargetElement,AllowCache) {
 function Each(obj,action) {
 	for (var key in obj){action(key,obj[key])};
 }
+
