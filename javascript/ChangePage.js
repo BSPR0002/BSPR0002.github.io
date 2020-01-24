b2ca06c151e4a8880310a9d9143b587682a51641
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2020-01-24 23:31:11 +0800

    小调整
    放弃制作loading提示

--- javascript/ChangePage.js
+++ javascript/ChangePage.js
@@ -1,12 +1,12 @@
 var sheet="home_page";
 
 document.addEventListener("DOMContentLoaded",function() {
-	Load("/html/home_page.html",document.getElementById("PageBox"));
+	Load("/html/home_page.html",document.getElementById("page_box"));
 });
 
 function ChangePage(page) {
 	if (page!=sheet) {
-		Load("/html/"+page+".html",document.getElementById("PageBox"));
+		Load("/html/"+page+".html",document.getElementById("page_box"));
 		sheet=page;
 	}
 }
