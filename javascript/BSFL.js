66bdbb5a20dac730d40bea3c56da914390cfa0c8
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2020-05-05 12:23:45 +0800

    Update BSFL.js

--- javascript/BSFL.js
+++ javascript/BSFL.js
@@ -356,7 +356,7 @@ var Base64={
 		if (!(typeof data=="object"&&data instanceof ArrayBuffer)) throw new Error("Base64 encoder accepts only ArrayBuffer objects.");
 		var table=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","+","/"];
 		var operator=new Uint8Array(data);
-		var result=padding=bits="";
+		var result="",padding="",bits="";
 		for (let byte=0,bytes=operator.length;byte<bytes;byte++) {
 			let temp=operator[byte].toString(2);
 			for (null;temp.length<8;temp="0"+temp);
