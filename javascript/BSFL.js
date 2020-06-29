3bb66ec4cd260f27a71fa6437bda9b67979cbd83
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2020-06-30 05:27:33 +0800

    音频功能转正

--- javascript/BSFL.js
+++ javascript/BSFL.js
@@ -379,3 +379,81 @@ var Base64={
 		return operator.buffer
 	}
 };
+
+class AudioPlayer {
+	//if (!(this instanceof AudioPlayer)) throw new Error("不合规范的调用！");
+	constructor() {
+		var audioContext=new window.AudioContext;
+		var analyser=audioContext.createAnalyser();
+		var gainNode=audioContext.createGain();
+		analyser.connect(audioContext.destination);
+		gainNode.connect(audioContext.destination);
+		this.audioContext=audioContext;
+		this.analyser=analyser;
+		this.gainNode=gainNode;
+		gainNode.gain.value=0;
+		Object.defineProperty(this,"volume",{
+			"get":function(){return Math.round((gainNode.gain.value+1)*1000)/10},
+			"set":function(value){gainNode.gain.value=value/100-1},
+			"enumerable":true
+		});
+	}
+	linkAudio(AudioNode) {
+		if (!(AudioNode instanceof AudioScheduledSourceNode)) throw new Error("输入的参数不是音频源节点！")
+		AudioNode.connect(this.analyser);
+		AudioNode.connect(this.gainNode);
+		var controller={
+			"audioNode":AudioNode,
+			"start":function(){AudioNode.start()},
+			"stop":function(){AudioNode.stop()}
+		};
+		Object.defineProperty(controller,"onended",{
+			"get":function(){return AudioNode.onended},
+			"set":function(value){AudioNode.onended=value},
+			"enumerable":true
+		});
+		Object.defineProperty(controller,"detune",{
+			"get":function(){return AudioNode.detune.value},
+			"set":function(value){AudioNode.detune.value=value},
+			"enumerable":true
+		});
+		return controller
+	}
+	linkBuffer(AudioBuffer) {
+		var source=this.audioContext.createBufferSource();
+		source.buffer=AudioBuffer;
+		var controller=this.linkAudio(source)
+		for (let item of ["loop","loopStart","loopEnd"]) {
+			Object.defineProperty(controller,item,{
+				"get":function(){return controller.audioNode[item]},
+				"set":function(value){controller.audioNode[item]=value},
+				"enumerable":true
+			});
+		};
+		Object.defineProperty(controller,"speed",{
+			"get":function(){return controller.audioNode.playbackRate.value},
+			"set":function(value){controller.audioNode.playbackRate.value=value},
+			"enumerable":true
+		});
+		for (let item of [["pause",0],["resume",1]]) controller[item[0]]=function(){controller.audioNode.playbackRate.value=item[1]};
+		return controller
+	}
+	play(AudioBuffer,loop=false,loopStart=0,loopEnd=0) {
+		var audio=this.linkBuffer(AudioBuffer);
+		if (loop===true) {
+			audio.loop=true;
+			audio.loopStart=typeof loopStart=="number"?loopStart:0;
+			audio.loopEnd=typeof loopEnd=="number"?loopEnd:0;
+			if (audio.loopStart!=0&&audio.loopEnd==0) console.warn("设置的循环结束时间为 0，音频循环可能不会符合预期效果。");
+		};
+		audio.start();
+		return audio
+	}
+	async playFile(file,loop=false,loopStart=0,loopEnd=0) {
+		var buffer=await this.audioContext.decodeAudioData(await file.arrayBuffer());
+		return this.play(buffer,loop,loopStart,loopEnd)
+	}
+	pause(){this.audioContext.suspend()}
+	resume(){this.audioContext.resume()}
+	close(){this.audioContext.close()}
+}
--- javascript/TestFunction.js
+++ javascript/TestFunction.js
@@ -1,77 +1,32 @@
-class AudioPlayer {
-	constructor() {
-		var audioContext=new window.AudioContext;
-		var analyser=audioContext.createAnalyser();
-		var gainNode=audioContext.createGain();
-		analyser.connect(audioContext.destination);
-		gainNode.connect(audioContext.destination);
-		this.audioContext=audioContext;
-		this.analyser=analyser;
-		this.gainNode=gainNode;
-		gainNode.gain.value=0;
-		Object.defineProperty(this,"volume",{
-			"get":function(){return Math.round((gainNode.gain.value+1)*1000)/10},
-			"set":function(value){gainNode.gain.value=value/100-1},
-			"enumerable":true
-		});
-	}
-	linkAudio(AudioNode) {
-		if (!(AudioNode instanceof AudioScheduledSourceNode)) throw new Error("输入参数不是音频源节点！")
-		AudioNode.connect(this.analyser);
-		AudioNode.connect(this.gainNode);
-		var controller={
-			"audioNode":AudioNode,
-			"start":function(){AudioNode.start()},
-			"stop":function(){AudioNode.stop()}
-		};
-		Object.defineProperty(controller,"onended",{
-			"get":function(){return AudioNode.onended},
-			"set":function(value){AudioNode.onended=value},
-			"enumerable":true
-		});
-		Object.defineProperty(controller,"detune",{
-			"get":function(){return AudioNode.detune.value},
-			"set":function(value){AudioNode.detune.value=value},
-			"enumerable":true
-		});
-		return controller
-	}
-	linkBuffer(AudioBuffer) {
-		var source=this.audioContext.createBufferSource();
-		source.buffer=AudioBuffer;
-		var controller=this.linkAudio(source)
-		for (let item of ["loop","loopStart","loopEnd"]) {
-			Object.defineProperty(controller,item,{
-				"get":function(){return controller.audioNode[item]},
-				"set":function(value){controller.audioNode[item]=value},
-				"enumerable":true
-			});
-		};
-		Object.defineProperty(controller,"speed",{
-			"get":function(){return controller.audioNode.playbackRate.value},
-			"set":function(value){controller.audioNode.playbackRate.value=value},
-			"enumerable":true
-		});
-		for (let item of [["pause",0],["resume",1]]) controller[item[0]]=function(){controller.audioNode.playbackRate.value=item[1]};
-		return controller
-	}
-	play(AudioBuffer,loop=false,loopStart=0,loopEnd=0) {
-		var audio=this.linkBuffer(AudioBuffer);
-		if (loop===true) {
-			audio.loop=true;
-			audio.loopStart=typeof loopStart=="number"?loopStart:0;
-			audio.loopEnd=typeof loopEnd=="number"?loopEnd:0;
-			if (audio.loopStart!=0&&audio.loopEnd==0) console.warn("设置的循环结束时间为 0，音频循环可能不会符合预期效果。");
-		};
-		audio.start();
-		return audio
-	}
-	async playFile(file,loop=false,loopStart=0,loopEnd=0) {
-		var buffer=await this.audioContext.decodeAudioData(await file.arrayBuffer());
-		return this.play(buffer,loop,loopStart,loopEnd)
-	}
-	pause(){this.audioContext.suspend()}
-	resume(){this.audioContext.resume()}
-	close(){this.audioContext.close()}
-}
+var ap,ac;
 
+async function test() {
+	var canvas=document.getElementById("canvas");
+	var context=canvas.getContext("2d");
+	var canvas2=document.getElementById("canvas2");
+	var context2=canvas2.getContext("2d");
+	context.strokeStyle="rgba(255,0,0,1)";
+	ap=new AudioPlayer;
+	ac=await ap.playFile(document.getElementById("input_file").files[0],true,34,180);
+	var data=new Uint8Array(1024);
+	function draw() {
+		ap.analyser.getByteFrequencyData(data);
+		context.clearRect(0,0,1024,256);
+		context.beginPath();
+		context.moveTo(0,255-data[0]);
+		for (let x=1;x<1024;x++) context.lineTo(x,255-data[x]);
+		context.stroke();
+	}
+	setInterval(draw,10);
+	context2.strokeStyle="rgba(255,0,0,1)";
+	var data2=new Uint8Array(2048);
+	function draw2() {
+		ap.analyser.getByteTimeDomainData(data2);
+		context2.clearRect(0,0,1024,256);
+		context2.beginPath();
+		context2.moveTo(0,255-data2[0]);
+		for (let x=1;x<1024;x++) context2.lineTo(2*x,255-data2[2*x]);
+		context2.stroke();
+	}
+	setInterval(draw2,10);
+}
