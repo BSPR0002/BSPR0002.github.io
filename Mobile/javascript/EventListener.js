da2b3f1d3336d5a14b2fd13dc8c003ab53fbd863
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2020-10-23 14:19:00 +0800

    重置

--- Mobile/javascript/EventListener.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("top_sheet_1").addEventListener("click",function(){ChangeSheet(1)});
-document.getElementById("top_sheet_2").addEventListener("click",function(){ChangeSheet(2)});
-document.getElementById("window_board_close").addEventListener("click",window_board.hide);
-ChangeSheet(1); //加载主页
-
-window.addEventListener("load",function() {
-	
-},{"once":true});
--- README.md
+++
@@ -1,4 +0,0 @@
-# Blue Sky Information Factory Official Website
-Here is BSIF Website's data base.
-
-[Blue Sky Information Factory Official Website](https://BSPR0002.github.io/)
