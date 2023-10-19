6a63adaa4688638e4f4201092905a884bc0f0fa8
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2023-10-20 07:49:21 +0800

    AH升级
    添加事件绑定功能

--- javascript/module/ArrayHTML.d.mts
+++ javascript/module/ArrayHTML.d.mts
@@ -1,10 +1,22 @@
+const EVENT_LISTENERS: symbol = Symbol("event listeners property");
+type AddEventListenerParameters<T extends keyof HTMLElementEventMap> = [
+	T,
+	((this: HTMLElement, event: HTMLElementEventMap[T]) => any) | EventListenerObject,
+	(boolean | AddEventListenerOptions)?
+];
+type CustomEventListenerParameters = [
+	string,
+	((this: HTMLElement, event: Event) => any) | EventListenerObject,
+	(boolean | AddEventListenerOptions)?
+]
 type ArrayHTMLNode = [
 	keyof HTMLElementTagNameMap | "#comment" | "#text" | "#shadow",
 	(ArrayHTMLCollection | string | boolean | number | bigint | Node)?,
 	{
-		[key: string]: string | any,
-		style?: { [key in keyof CSSStyleDeclaration]: string } | string,
-		class?: string[] | string
+		style?: Partial<CSSStyleDeclaration> | string,
+		class?: string[] | string,
+		[EVENT_LISTENERS]?: (AddEventListenerParameters<keyof HTMLElementEventMap> | CustomEventListenerParameters)[],
+		[key: string]: any
 	}?,
 	string?
 ];
@@ -15,4 +27,4 @@ declare function parseAndGetNodes(ArrayHTML: ArrayHTMLCollection): {
 	documentFragment: DocumentFragment,
 	nodes: { [key: string]: HTMLElementTagNameMap[keyof HTMLElementTagNameMap] | Comment | Text | ShadowRoot };
 };
-export { parse, serialize, parseAndGetNodes }
+export { parse, serialize, parseAndGetNodes, EVENT_LISTENERS }
--- javascript/module/ArrayHTML.mjs
+++ javascript/module/ArrayHTML.mjs
@@ -1,5 +1,6 @@
-function parseCollection(data,outer,collector) {
-	if (!Array.isArray(data)) throw new TypeError("Parse error: Detected non-Array object");
+const EVENT_LISTENERS = Symbol("event listeners property");
+function parseCollection(data, outer, collector) {
+	if (!Array.isArray(data)) throw new TypeError("Parse error: Non-Array object");
 	for (let item of data) {
 		switch (typeof item) {
 			case "string":
@@ -14,37 +15,37 @@ function parseCollection(data,outer,collector) {
 					outer.appendChild(item);
 					break;
 				}
-				parseNode(item,outer,collector)
+				parseNode(item, outer, collector)
 			default:
 		}
 	}
 }
-function parseStyleObject(node,object) {
-	const styleObject=node.style;
-	for (let index in object) styleObject[index]=[object[index]];
+function parseStyleObject(node, object) {
+	const styleObject = node.style;
+	for (let index in object) styleObject[index] = [object[index]];
 }
-function parseAttribute(node,attributeName,data) {
+function parseAttribute(node, attributeName, data) {
 	switch (attributeName) {
 		case "style":
 			if (data instanceof Object) {
-				parseStyleObject(node,data);
+				parseStyleObject(node, data);
 				return;
 			}
 			break;
 		case "class":
-			node.setAttribute("class",Array.isArray(data)?data.join(" "):data);
+			node.setAttribute("class", Array.isArray(data) ? data.join(" ") : data);
 			return;
 		default:
 	}
-	node.setAttribute(attributeName,data)
+	node.setAttribute(attributeName, data)
 }
-function parseContent(node,content,collector) {
+function parseContent(node, content, collector) {
 	switch (typeof content) {
 		case "string":
 		case "number":
 		case "bigint":
 		case "boolean":
-			node.innerText=content;
+			node.innerText = content;
 			return;
 		case "object":
 			if (!content) return;
@@ -52,88 +53,84 @@ function parseContent(node,content,collector) {
 				node.appendChild(content);
 				return;
 			}
-			try {parseCollection(content,node,collector)} catch(error){console.warn(error)}
+			parseCollection(content, node, collector);
 		default:
 	}
 }
-function parseNode(data,outer,collector) {
-	if (!Array.isArray(data)) throw new TypeError("Parse error: Detected non-Array object");
-	var content=data[1],node;
+function parseNode(data, outer, collector) {
+	if (!Array.isArray(data)) throw new TypeError("Parse error: Non-Array object");
+	var content = data[1], node;
 	switch (data[0]) {
 		case "#comment":
-			outer.appendChild(node=document.createComment(content));
+			outer.appendChild(node = document.createComment(content));
 			break;
 		case "#text":
-			outer.appendChild(node=document.createTextNode(content));
+			outer.appendChild(node = document.createTextNode(content));
 			break;
 		case "#shadow":
+			if (!(outer instanceof Element)) throw new TypeError("Container is not an Element.");
 			try {
-				if (!(outer instanceof Element)) throw new TypeError("Container is not an Element.")
-				node=outer.attachShadow(data[2])
-			} catch(error) {
-				console.warn("Parse error: Failed to attach shadow DOM\n",data,"\non",outer,`\n${error.name}: ${error.message}`);
+				node = outer.attachShadow(data[2])
+			} catch (error) {
+				console.warn("Parse error: Failed to attach shadow DOM\n", data, "\non", outer, `\n${error.name}: ${error.message}`);
 				return;
 			}
-			parseContent(node,content,collector);
+			parseContent(node, content, collector);
 			break;
 		default:
-			outer.appendChild(node=document.createElement(data[0]));
-			if (data[2] instanceof Object) {
-				for (let attribute in data[2]) try {parseAttribute(node,attribute,data[2][attribute])} catch (error) {console.warn("Parse error: Failed to set attribute",attribute,"to",data[2][attribute],"on",node,`\n${error.name}: ${error.message}`)}
+			outer.appendChild(node = document.createElement(data[0]));
+			const attributes = data[2]
+			if (attributes instanceof Object) {
+				for (const attribute in attributes) try { parseAttribute(node, attribute, attributes[attribute]) } catch (error) { console.warn("Parse error: Failed to set attribute", attribute, "to", attributes[attribute], "on", node, `\n${error.name}: ${error.message}`) };
+				if (EVENT_LISTENERS in attributes) for (const item of attributes[EVENT_LISTENERS]) node.addEventListener(...item);
 			}
-			parseContent(node,content,collector);
+			parseContent(node, content, collector);
 	}
-	if (collector&&(3 in data)) collector[data[3]]=node;
+	if (collector && (3 in data)) collector[data[3]] = node;
 }
 function parse(ArrayHTML) {
-	const documentFragment=document.createDocumentFragment();
-	parseCollection(ArrayHTML,documentFragment);
+	const documentFragment = document.createDocumentFragment();
+	parseCollection(ArrayHTML, documentFragment);
 	return documentFragment;
 }
 function parseAndGetNodes(ArrayHTML) {
-	const nodes={},documentFragment=document.createDocumentFragment();
-	parseCollection(ArrayHTML,documentFragment,nodes);
-	return {documentFragment,nodes};
+	const nodes = {}, documentFragment = document.createDocumentFragment();
+	parseCollection(ArrayHTML, documentFragment, nodes);
+	return { documentFragment, nodes };
 }
-function serializeIterator(node,outer) {
-	if (node.nodeName=="#text") {
+function serializeIterator(node, outer) {
+	if (node.nodeName == "#text") {
 		outer.push(node.textContent);
-	} else {
-		for (let child of node.childNodes) {serializeNode(child,outer)}
-	}
+	} else for (let child of node.childNodes) serializeNode(child, outer);
 }
-function serializeNode(node,outer){
+function serializeNode(node, outer) {
 	switch (node.nodeName) {
 		case "#text":
 			outer.push(node.textContent);
 			break;
 		case "#comment":
-			outer.push(["#comment",node.textContent]);
+			outer.push(["#comment", node.textContent]);
 			break;
 		case "#document":
-			serializeIterator(node,outer);
+			serializeIterator(node, outer);
 		case "html":
 			break;
 		default:
-			let child=[node.localName];
+			let child = [node.localName];
 			try {
 				if (node.hasAttributes()) {
-					child[2]={};
-					for (let attribute of node.attributes) {
-						child[2][attribute.name]=attribute.value;
-					}
+					const attributes = child[2] = {};
+					for (let attribute of node.attributes) attributes[attribute.name] = attribute.value;
 				}
-			} catch(error) {console.warn("Serialize exception: Failed to get attributes of node",node)}
-			if (node.hasChildNodes()) {
-				serializeIterator(node,child[1]=[]);
-			}
+			} catch (error) { console.warn("Serialize exception: Failed to get attributes of node", node) }
+			if (node.hasChildNodes()) serializeIterator(node, child[1] = []);
 			outer.push(child);
 	}
 }
-function serialize(node,onlyChildren=true) {
+function serialize(node, onlyChildren = true) {
 	if (!(node instanceof Node)) throw new TypeError("Serialize failed: Argument 'node' is not type of Node");
-	const ArrayHtml=[];
-	if (onlyChildren) {serializeIterator(node,ArrayHtml)} else {serializeNode(node,ArrayHtml)}
+	const ArrayHtml = [];
+	if (onlyChildren) { serializeIterator(node, ArrayHtml) } else { serializeNode(node, ArrayHtml) }
 	return ArrayHtml;
 }
-export {parse,serialize,parseAndGetNodes}
+export { parse, serialize, parseAndGetNodes, EVENT_LISTENERS }
