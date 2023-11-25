f3fd33b5d0fa037ec79994c4e784f035e6bd8050
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2023-11-26 01:09:45 +0800

    分离应用

--- application/json_indexed_editor/data.mjs
+++
@@ -1,6 +0,0 @@
-import IndexedDatabase from "/javascript/module/IndexedDatabase.mjs";
-const database = await IndexedDatabase.open("org.BSIF.JSONIndexedEditor", 1, upgrader => {
-	upgrader.createObjectStore("UI");
-	upgrader.createObjectStore("indexors", { keyPath: "name" });
-});
-export default database;
--- application/json_indexed_editor/menu.mjs
+++
@@ -1,9 +0,0 @@
-import { EVENT_LISTENERS, parse } from "/javascript/module/ArrayHTML.mjs";
-const menu = document.getElementById("menu");
-var focused = false;
-function buildMenu(array) { menu.appendChild(parse(array.map(menuMapper1))) }
-function focusMenu() { if (menu.contains(document.activeElement)) this.focus() }
-function clickOption({ target }) { if (target != this) target.blur() }
-function menuMapper1(item, tabindex) { return ["div", [item.title, ["div", item.options.map(menuMapper2), { class: "menu_expand" }]], { class: "menu_item", tabindex, [EVENT_LISTENERS]: [["mouseenter", focusMenu], ["click", clickOption]] }] }
-function menuMapper2(item) { return ["button", item.title, { class: "menu_option", [EVENT_LISTENERS]: [["click", item.action]] }] }
-export { buildMenu };
