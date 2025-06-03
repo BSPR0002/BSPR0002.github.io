a01980f120a8802e14381d475331cd22c5863624
BSPR0002 <42113148+BSPR0002@users.noreply.github.com>
2025-06-03 10:36:40 +0800

    service worker 支持升级

--- application/support.mjs
+++ application/support.mjs
@@ -1,42 +1,61 @@
-const scope = registration.scope, ownCachePrefix = new URL(scope).pathname + ":";
-function ownUrl(fileUrl) { return scope + fileUrl }
-function requiredScriptsUrl(fileUrl) { return "/javascript/module/" + fileUrl }
+/** @ts-ignore @type {ServiceWorkerRegistration} */
+const {scope} = registration, cacheNamePrefix = new URL(scope).pathname + ":",
+	scopeMapping = {
+		scriptModule: "/javascript/module/",
+		component: "/component/",
+		website: "/"
+	};
+/**
+ * @param {string[]} list
+ * @param {string} scope
+ * @param {string[]} paths
+ */
+function addScopeUrls(list, scope, paths) { for (const item of paths) list.push(scope + item) }
 class CacheController {
+	/**
+	 * @param {string | number} version 
+	 */
 	constructor(version) {
-		const ownCacheName = ownCachePrefix + version;
+		this.cacheName = cacheNamePrefix + (this.version = version);
 		Object.defineProperties(this, {
-			ownCacheName: { enumerable: true, value: ownCacheName },
-			cacheScopes: {
-				enumerable: true, value: Object.freeze([
-					{ cacheName: ownCacheName, scope },
-					{ cacheName: "/javascript/module/", scope: origin + "/javascript/module/" },
-					{ cacheName: "/", scope: origin + "/" }
-				])
-			},
-			version: { enumerable: true, value: version }
-		})
+			ownCacheName: { enumerable: true },
+			version: { enumerable: true }
+		});
 	}
+	/**
+	 * @param {{ own: string[], scriptModule?: string[], website?: string[], component?: string[] }} resources
+	 */
 	async install(resources) {
-		const { own, requiredScripts, shared } = resources;
-		await (await caches.open(this.ownCacheName)).addAll(own.map(ownUrl));
-		if (requiredScripts) await (await caches.open("/javascript/module/")).addAll(requiredScripts.map(requiredScriptsUrl));
-		if (shared) await (await caches.open("/")).addAll(shared);
+		const { own } = resources, cache = await caches.open(this.cacheName), list = [];
+		addScopeUrls(list, scope, own);
+		for (const key in scopeMapping)
+			if (key in resources)
+				addScopeUrls(list, scopeMapping[key], resources[key]);
+		await cache.addAll(list);
 		console.log("Service worker installed successfully.\nResource version: " + this.version);
 	}
+	/**
+	 * @param {Request} request
+	 */
 	async respond(request) {
-		const url = request.url;
-		if (request.mode == "navigate") return url != scope && url != scope + "index.html" ?
-			new Response(null, { status: 301, headers: { Location: scope } }) :
-			await (await caches.open(this.ownCacheName)).match(scope + "index.html");
-		for (const { scope, cacheName } of this.cacheScopes) {
-			if (!url.startsWith(scope)) continue;
-			const response = await (await caches.open(cacheName)).match(url);
-			if (response) return response;
-			break;
+		const url = request.url, cache = await caches.open(this.cacheName);
+		if (request.mode == "navigate") {
+			const indexFile = scope + "index.html";
+			return url != scope && url != indexFile ?
+				new Response(null, { status: 301, headers: { Location: scope } }) :
+				await cache.match(indexFile);
 		}
-		return fetch(request);
+		return await cache.match(url) ?? fetch(request);
 	}
-	async clean() { for (const cacheName of await caches.keys()) if (cacheName.startsWith(ownCachePrefix) && cacheName != this.ownCacheName) await caches.delete(cacheName) }
-	async uninstall(){for (const cacheName of await caches.keys()) if (cacheName.startsWith(ownCachePrefix)) await caches.delete(cacheName)}
+	async clean() {
+		for (const cacheName of await caches.keys())
+			if (cacheName.startsWith(cacheNamePrefix) && cacheName != this.cacheName)
+				await caches.delete(cacheName)
+	}
+	async uninstall() {
+		for (const cacheName of await caches.keys())
+			if (cacheName.startsWith(cacheNamePrefix))
+				await caches.delete(cacheName)
+	}
 }
 export default CacheController;
